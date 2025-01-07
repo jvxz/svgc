8ff76a104f68123a299a4d3b7ca1fdb990926dcb
jamie 🌨️ <jamie@wisp.bio>
2025-01-07 12:56:01 -0500

    feat: :sparkles: add ThemeToggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${radioCanadaBig.variable} ${dmSerifDisplay.variable}`}
     >
