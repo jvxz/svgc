04d9f389e497c32f6dbdd677c859383c530e687b
jamie 🌨️ <jamie@wisp.bio>
2025-01-07 18:04:19 -0500

    refactor: :lipstick: update Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,16 @@
+import { ThemeToggle } from "../ThemeToggle";
+import { Input } from "../ui/input";
+
 function Navbar() {
-  return <nav className="h-16 border-b border-border">Navbar</nav>;
+  return (
+    <nav className="grid h-16 grid-cols-3 items-center border-b border-border px-8">
+      <p className="font-mono text-2xl font-bold">svgc</p>
+      <Input placeholder="Search..." />
+      <div className="flex justify-end">
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
 }
 
 export { Navbar };
