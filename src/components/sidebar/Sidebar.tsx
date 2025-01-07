f87b32f683fa7d31144b90c2c7609b2a4b1cb50a
jamie 🌨️ <jamie@wisp.bio>
2025-01-07 18:04:34 -0500

    feat: :sparkles: add search to sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
 import { type iSVG } from "@/actions/get-svgs";
 import { Badge } from "../ui/badge";
 import { Toggle } from "../ui/toggle";
+import { ScrollArea } from "../ui/scroll-area";
+import SidebarSearch from "./SidebarSearch";
 
 function Sidebar({ svgs }: { svgs: iSVG[] }) {
   const getCategories = () => {
@@ -21,18 +23,23 @@ function Sidebar({ svgs }: { svgs: iSVG[] }) {
 
   return (
     <aside className="min-w-[20%] border-r border-border">
-      <div className="flex flex-col gap-2 p-4">
-        {categories.map((category) => (
-          <Toggle
-            className="flex items-center justify-between"
-            key={category.name}
-            variant="outline"
-          >
-            {category.name}
-            <Badge variant="outline">{category.count}</Badge>
-          </Toggle>
-        ))}
+      <div className="h-[4.25rem] p-4">
+        <SidebarSearch />
       </div>
+      <ScrollArea className="h-[calc(100%-4.25rem)]">
+        <div className="flex flex-col gap-2 p-4">
+          {categories.map((category) => (
+            <Toggle
+              className="flex items-center justify-between"
+              key={category.name}
+              variant="outline"
+            >
+              {category.name}
+              <Badge variant="outline">{category.count}</Badge>
+            </Toggle>
+          ))}
+        </div>
+      </ScrollArea>
     </aside>
   );
 }
