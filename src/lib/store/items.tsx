4083fd5b464a532c8a571083de7b8070a40f0cc3
jamie 🌨️ <jamie@wisp.bio>
2025-01-12 18:06:25 -0500

    feat: :sparkles: added persistence for items & view mode

--- src/lib/store/items.tsx
+++ src/lib/store/items.tsx
@@ -1,5 +1,6 @@
 import { type Logo, type LogoList } from "@/actions/get-svgs";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ItemsStore {
   items: LogoList;
@@ -9,20 +10,27 @@ interface ItemsStore {
   addItem: (item: Logo) => void;
   setSelectedItemIndex: (index: number) => void;
 }
-export const useItemsStore = create<ItemsStore>((set) => ({
-  items: [],
-  addItem: (item) =>
-    set((state) => {
-      if (state.items.find((i) => i.name === item.name)) {
-        return state;
-      }
-      return { items: [...state.items, item] };
+export const useItemsStore = create<ItemsStore>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (item) =>
+        set((state) => {
+          if (state.items.find((i) => i.name === item.name)) {
+            return state;
+          }
+          return { items: [...state.items, item] };
+        }),
+      removeItem: (item) =>
+        set((state) => ({
+          items: state.items.filter((i) => i.name !== item.name),
+        })),
+      selectedItemIndex: 0,
+      setSelectedItemIndex: (index) => set({ selectedItemIndex: index }),
+      clearItems: () => set({ items: [] }),
     }),
-  removeItem: (item) =>
-    set((state) => ({
-      items: state.items.filter((i) => i.name !== item.name),
-    })),
-  selectedItemIndex: 0,
-  setSelectedItemIndex: (index) => set({ selectedItemIndex: index }),
-  clearItems: () => set({ items: [] }),
-}));
+    {
+      name: "items",
+    },
+  ),
+);
--- src/lib/store/svg-view-mode.tsx
+++ src/lib/store/svg-view-mode.tsx
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface SVGViewMode {
   viewMode: string;
   setViewMode: (val: string) => void;
 }
 
-export const useSVGViewMode = create<SVGViewMode>((set) => ({
-  viewMode: "grid",
-  setViewMode: (val) => set(() => ({ viewMode: val })),
-}));
+export const useSVGViewMode = create<SVGViewMode>()(
+  persist(
+    (set) => ({
+      viewMode: "grid",
+      setViewMode: (val) => set(() => ({ viewMode: val })),
+    }),
+    {
+      name: "svg-view-mode",
+    },
+  ),
+);
