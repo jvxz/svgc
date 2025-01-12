46ee79528515b3ccc935133542ceb9b940735ffc
jamie 🌨️ <jamie@wisp.bio>
2025-01-12 14:21:54 -0500

    feat: :sparkles: add 404 redirect, add links to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,32 @@
+import { Info } from "lucide-react";
 import Link from "next/link";
 import { ThemeToggle } from "../ThemeToggle";
+import { Button } from "../ui/button";
+import { Icon } from "../ui/logos";
 import ItemsButton from "./ItemsButton";
 
 function Navbar() {
   return (
-    <nav className="grid min-h-16 grid-cols-3 items-center border-b border-border px-8">
+    <nav className="grid min-h-16 grid-cols-2 items-center border-b border-border px-8">
       <Link
         href="/"
         className="w-fit font-mono text-2xl font-bold underline-offset-2 hover:underline"
       >
         svgc
       </Link>
-      <div className="flex justify-center text-sm text-muted-foreground">
-        <p>made with ❤️</p>
-      </div>
+
       <div className="flex justify-end gap-2">
         <ItemsButton />
+        <Button size="icon" variant="outline">
+          <Link href="https://github.com/jvxz/svgc">
+            <Icon.Github />
+          </Link>
+        </Button>
+        <Button size="icon" variant="outline">
+          <Link href="/info">
+            <Info />
+          </Link>
+        </Button>
         <ThemeToggle />
       </div>
     </nav>
