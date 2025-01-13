499196ba7a5a52c9b2720e2535aa51961ecf88f1
jamie 🌨️ <jamie@wisp.bio>
2025-01-12 19:10:34 -0500

    refactor: :recycle: edit icons.json, revert server action

--- src/actions/get-items-code.ts
+++ src/actions/get-items-code.ts
@@ -13,8 +13,12 @@ async function getItemsCode(items: Logo[], mode: FormatSvgMode) {
             name: "convertColors",
             params: { currentColor: !mode.retainBrandColors },
         },
-        { name: "removeXMLNS" },
-        { name: "removeXlink" },
+        {
+            name: "removeXMLNS",
+        },
+        {
+            name: "removeXlink"
+        },
     ] as PluginConfig[];
 
     try {
@@ -33,59 +37,45 @@ async function getItemsCode(items: Logo[], mode: FormatSvgMode) {
             }),
         );
 
-        // format component name
-        const formatComponentName = (name: string) => {
-            return name
-                // capitalize first letter
-                .charAt(0)
-                .toUpperCase() +
-                // remove spaces, dots, slashes, and capitalize js, ui
-                name.slice(1)
-                    .replace(/\s+/g, "")
-                    .replace(".", "")
-                    .replace("/", "")
-                    .replace("js", "JS")
-                    .replace("ui", "UI");
-        };
-
-        // generate component props
-        const getComponentProps = (addSizeProps: boolean) => {
-            if (!addSizeProps) return "";
-            return `{
-                width = 64,
-                height = 64, 
-                size = 64,
-                ...props
-            }: SVGProps<SVGSVGElement> & { size?: number }`;
-        };
-
-        // generate initial code
         let code = `
-            ${mode.addSizeProps ? 'import { type SVGProps } from "react";' : ""}
+      ${mode.addSizeProps ? 'import { type SVGProps } from "react";' : ""}
 
-            const Icon = {
-                ${formatted.map(item => {
-            const name = formatComponentName(item.name);
-            const props = getComponentProps(mode.addSizeProps ?? false);
+      const Icon = {
+        ${formatted
+                .map(
+                    (item) =>
+                        `${item.name.replace(/\s+/g, "")
+                            .replace(/[^a-zA-Z0-9 ]/g, "")
+                            .replace("js", "JS")
+                            .replace("ui", "UI")
+                        }: (${mode.addSizeProps
+                            ? `{
+              width = 64,
+              height = 64,
+              size = 64,
+              ...props
+            }: SVGProps<SVGSVGElement> & { size?: number }`
+                            : ""
+                        }) => {
+              return (
+                ${item.svg}
+              )
+            },`,
+                )
+                .join("\n")}
+      }
+      
+      export { Icon };
+    `;
 
-            return `${name}: (${props}) => {
-                        return (
-                            ${item.svg}
-                        )
-                    },`;
-        }).join("\n")}
-            }
-
-            export { Icon };
-        `;
-
-        // add size props if needed
-        if (mode.addSizeProps) {
+        if (mode.addSizeProps)
             code = code
                 .replace(/width="[0-9]+"/g, "width={size ?? width}")
-                .replace(/height="[0-9]+"/g, `height={size ?? height}
-                    {...props}`);
-        }
+                .replace(
+                    /height="[0-9]+"/g,
+                    `height={size ?? height}
+          {...props}`,
+                );
 
         const formattedCode = await formatSvg(code);
         return formattedCode;
