5dfe495a40dafd21550a218070ace7dc40df833e
jamie 🌨️ <jamie@wisp.bio>
2025-01-13 01:24:02 -0500

    feat: :sparkles: implement typewriter effect in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,25 @@
+import { Typewriter } from "@/components/ui/typewriter";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <main className="container motion-preset-fade mx-auto my-10 max-w-[600px]">
+      <div className="flex flex-col gap-2">
+        <Typewriter
+          loop={false}
+          speed={250}
+          cursorChar={""}
+          text="svgc"
+          className="w-full text-center font-mono text-4xl font-bold"
+        />
+        <Typewriter
+          initialDelay={1150}
+          loop={false}
+          speed={50}
+          cursorChar={""}
+          text="Simple icon component generator for React"
+          className="w-full text-center"
+        />
+      </div>
+    </main>
+  );
 }
