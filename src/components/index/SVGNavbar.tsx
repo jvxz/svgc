810e8012ca7d8b35aeb377c1d280d9b85783b0a5
jamie 🌨️ <jamie@wisp.bio>
2025-01-18 18:12:45 -0500

    refactor: :recycle: refactor SVGNavbar

--- src/components/index/SVGNavbar.tsx
+++ src/components/index/SVGNavbar.tsx
@@ -7,8 +7,20 @@ import { SVGSortBy } from "./SVGSortBy";
 import { SVGViewAs } from "./SVGViewAs";
 
 export function SVGNavbar() {
-  const { searchInput, setSearchInput } = useInputStore();
+  return (
+    <div
+      className={`absolute top-0 z-10 flex w-full flex-1 items-center gap-2 border-b border-border bg-background/70 backdrop-blur-lg ${NAVBARS_HEIGHT} p-4`}
+    >
+      <SVGViewAs />
+      <SVGSortBy />
+      <SVGSearchInput />
+    </div>
+  );
+}
+
+function SVGSearchInput() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const { searchInput, setSearchInput } = useInputStore();
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -25,25 +37,19 @@ export function SVGNavbar() {
   }, []);
 
   return (
-    <div
-      className={`absolute top-0 z-10 flex w-full flex-1 items-center gap-2 border-b border-border bg-background/70 backdrop-blur-lg ${NAVBARS_HEIGHT} p-4`}
-    >
-      <SVGViewAs />
-      <SVGSortBy />
-      <div className="relative flex-1">
-        <Input
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          ref={inputRef}
-          className="pe-11"
-          placeholder="Search..."
-          type="search"
-        />
-        <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-2 text-muted-foreground">
-          <kbd className="inline-flex h-5 max-h-full items-center rounded border border-border px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70">
-            ⌘K
-          </kbd>
-        </div>
+    <div className="relative flex-1">
+      <Input
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        ref={inputRef}
+        className="pe-11"
+        placeholder="Search..."
+        type="search"
+      />
+      <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-2 text-muted-foreground">
+        <kbd className="inline-flex h-5 max-h-full items-center rounded border border-border px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70">
+          ⌘K
+        </kbd>
       </div>
     </div>
   );
