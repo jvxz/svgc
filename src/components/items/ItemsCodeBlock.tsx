38fde268e35495d4acace9488a144b20571bbdb2
jamie 🌨️ <jamie@wisp.bio>
2025-01-18 19:58:50 -0500

    feat: :sparkles: enhance items page components

--- src/components/items/ItemsCodeBlock.tsx
+++ src/components/items/ItemsCodeBlock.tsx
@@ -1,5 +1,35 @@
+"use client";
+import { useSelectedItemsStore } from "@/lib/store/selected-items";
+import { ArchiveX } from "lucide-react";
+import { CodeBlock } from "react-code-block";
+
 function ItemsCodeBlock() {
-  return <div className="h-2/3"></div>;
+  const { selectedItems } = useSelectedItemsStore();
+  const noItems = selectedItems.length === 0;
+
+  const code = `console.log('Hello World!')`;
+
+  return (
+    <div className="h-2/3">
+      <CodeBlock code={code} language="ts">
+        <CodeBlock.Code className="h-full bg-black p-4">
+          {noItems && (
+            <div className="grid h-full place-items-center">
+              <ArchiveX size={75} className="text-muted" />
+            </div>
+          )}
+          {!noItems && (
+            <div className="flex items-center gap-4 tracking-tight">
+              <CodeBlock.LineNumber className="text-sm text-foreground/40" />
+              <CodeBlock.LineContent>
+                <CodeBlock.Token />
+              </CodeBlock.LineContent>
+            </div>
+          )}
+        </CodeBlock.Code>
+      </CodeBlock>
+    </div>
+  );
 }
 
 export { ItemsCodeBlock };
--- src/components/items/ItemsDisplay.tsx
+++ src/components/items/ItemsDisplay.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useSelectedItemsStore } from "@/lib/store/selected-items";
 import { getImageUrl } from "@/lib/utils";
+import { ArchiveX } from "lucide-react";
 import Image from "next/image";
 import { GridBackground } from "../GridBackground";
 
 function ItemsDisplay() {
   const { selectedItems } = useSelectedItemsStore();
+  const noItems = selectedItems.length === 0;
+
   const isMultipleSelected = selectedItems.length > 1;
   const imageUrl = selectedItems[0]?.files[0]
     ? getImageUrl(selectedItems[0]?.files[0])
@@ -14,9 +17,9 @@ function ItemsDisplay() {
   return (
     <div className="grid h-1/3 place-items-center border-b border-border">
       <GridBackground>
-        {isMultipleSelected ? (
-          <div>Multiple selected</div>
-        ) : (
+        {noItems && <ArchiveX size={75} className="text-muted-foreground" />}
+        {isMultipleSelected && !noItems && <div>Multiple selected</div>}
+        {!isMultipleSelected && !noItems && (
           <Image
             src={imageUrl}
             alt={selectedItems[0]?.name ?? ""}
