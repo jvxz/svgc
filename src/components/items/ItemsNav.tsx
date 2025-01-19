471d622cf291a90098b88fbe4fdb5458fde47e52
jamie 🌨️ <jamie@wisp.bio>
2025-01-18 20:52:18 -0500

    refactor: :lipstick: edit itemsnav

--- src/components/items/ItemsNav.tsx
+++ src/components/items/ItemsNav.tsx
@@ -1,15 +1,50 @@
 "use client";
-
 import { useIsItemsEmpty, useItemsStore } from "@/lib/store/items";
 import {
   setSelectedItem,
   useSelectedItemsStore,
 } from "@/lib/store/selected-items";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, ExternalLink } from "lucide-react";
+import Link from "next/link";
 import { useEffect } from "react";
 import { Button } from "../ui/button";
 
 function ItemsNav() {
+  const { selectedItems } = useSelectedItemsStore();
+  const { items } = useItemsStore();
+  const isEmpty = useIsItemsEmpty();
+  const multipleSelected = selectedItems.length > 1;
+
+  useEffect(() => {
+    if (items.length === 0 || items[0] === undefined) return;
+    if (selectedItems.length === 0) setSelectedItem(items[0]?.data);
+  }, [isEmpty, items, selectedItems.length]);
+
+  return (
+    <div className="h-navbar flex items-center justify-between border-b border-border bg-background/70 p-4 backdrop-blur-lg">
+      {isEmpty && <p className="text-2xl">No items found</p>}
+
+      {!isEmpty && !multipleSelected && (
+        <Link
+          href={selectedItems[0]?.url ?? ""}
+          target="_blank"
+          className="flex items-center gap-2 text-2xl hover:underline"
+        >
+          {selectedItems[0]?.name ?? items[0]?.data.name}
+          <ExternalLink size={20} />
+        </Link>
+      )}
+
+      {!isEmpty && multipleSelected && (
+        <p className="text-2xl">{selectedItems.length} items selected</p>
+      )}
+
+      <ItemsNavButtons />
+    </div>
+  );
+}
+
+function ItemsNavButtons() {
   const { selectedItems } = useSelectedItemsStore();
   const { items } = useItemsStore();
   const isEmpty = useIsItemsEmpty();
@@ -26,43 +61,25 @@ function ItemsNav() {
     if (nextItem) setSelectedItem(nextItem.data);
   };
 
-  useEffect(() => {
-    if (items.length === 0 || items[0] === undefined) return;
-    if (selectedItems.length === 0) setSelectedItem(items[0]?.data);
-  }, [isEmpty, items, selectedItems.length]);
-
   return (
-    <div className="h-navbar flex items-center justify-between border-b border-border bg-background/70 p-4 backdrop-blur-lg">
-      {isEmpty && (
-        <p className="text-2xl text-muted-foreground">No items found</p>
-      )}
-
-      {!isEmpty && (
-        <p className="text-2xl text-muted-foreground">
-          {multipleSelected
-            ? `${selectedItems.length} items selected`
-            : `${selectedItems[0]?.name ?? items[0]?.data.name}`}
-        </p>
-      )}
-
-      <div className="flex items-center gap-2">
-        <Button
-          disabled={isEmpty || multipleSelected}
-          variant="outline"
-          size="icon"
-          onClick={() => handleNextItem("prev")}
-        >
-          <ArrowLeft />
-        </Button>
-        <Button
-          disabled={isEmpty || multipleSelected}
-          variant="outline"
-          size="icon"
-          onClick={() => handleNextItem("next")}
-        >
-          <ArrowRight />
-        </Button>
-      </div>
+    <div className="flex items-center gap-2">
+      <Button
+        disabled={isEmpty || multipleSelected}
+        variant="outline"
+        size="icon"
+        onClick={() => handleNextItem("prev")}
+      >
+        <ArrowLeft />
+      </Button>
+      <Button
+        disabled={isEmpty || multipleSelected}
+        variant="outline"
+        size="icon"
+        onClick={() => handleNextItem("next")}
+      >
+        <ArrowRight />
+      </Button>
+      <Button>Export</Button>
     </div>
   );
 }
