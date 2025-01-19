f9580dd9ef783062224b1c31e4173c16feb511cf
jamie 🌨️ <jamie@wisp.bio>
2025-01-18 20:43:13 -0500

    chore: :pushpin: manage deps

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "next-themes";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { TailwindIndicator } from "./tailwind-indicator";
 import { TooltipProvider } from "./ui/tooltip";
 const queryClient = new QueryClient({
@@ -17,22 +17,21 @@ const queryClient = new QueryClient({
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NuqsAdapter>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <TooltipProvider>
-            <TailwindIndicator />
-            <SpeedInsights />
-            {children}
-          </TooltipProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </NuqsAdapter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <TooltipProvider>
+          <TailwindIndicator />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <SpeedInsights />
+          {children}
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
